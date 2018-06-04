2118a6ca81ad2acee5ffcc64ba397630a56f58b3
Jeremie Roy <j-roy95@hotmail.com>
2018-06-04 16:40:40 -0400

    Start working on graphql

--- app/pods/beers/route.js
+++ app/pods/beers/route.js
@@ -1,16 +1,10 @@
 import Route from '@ember/routing/route';
-import query from 'pinte-ball/queries/get-beers';
-import { inject as service } from '@ember/service';
+import query from 'assets/pinte-ball/queries/get-beers.graphql';
 
 export default Route.extend({
-  apollo: service('apollo'),
+    apollo: Ember.inject.service(),
 
-  model() {
-    let variables = {
-      skip: 0,
-      first: 10
-    };
-
-    return this.get('apollo').query({query, variables}, "beers");
-  }
+    model() {
+        return this.get('apollo').query({query});
+    }
 });
