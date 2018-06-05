01a9dbf6b6bb746540ac73b5c8e08e4affe30176
Jeremie Roy <j-roy95@hotmail.com>
2018-06-05 17:56:37 -0400

    Working page with fake data

--- app/pods/beers/route.js
+++ app/pods/beers/route.js
@@ -1,6 +1,6 @@
 import Route from '@ember/routing/route';
 import RouteQueryManager from 'ember-apollo-client/mixins/route-query-manager';
-import query from '../../queries/get-beers';
+import query from 'pinte-ball/queries/get-beers';
 
 export default Route.extend(RouteQueryManager, {
     model() {
@@ -9,6 +9,6 @@ export default Route.extend(RouteQueryManager, {
           first: 10
         };
 
-        return this.get('apollo').query({ query, variables }, 'beers');
+        return [{id: 1, name: "Péché mortel", description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."}, {id: 2, name: "Coors light", description: "alalal"}, {id: 3, name: "Other", description: "alalal"}];
     }
 });
---
+++ app/pods/components/bp-beer-list/component.js
@@ -0,0 +1,16 @@
+import Component from '@ember/component';
+import { computed } from '@ember/object';
+
+export default Component.extend({
+    table: computed('beers', function() {
+        const beers = this.get('beers');
+        const nbRow = Math.round(beers.length / 2.0);
+        const table = [];
+        for(let i = 0; i < nbRow * 2; i+=2) {
+            let row = [beers[i], beers[i+1]];
+            console.log(row);
+            table.push(row);
+        }
+        return table;
+    })
+});
