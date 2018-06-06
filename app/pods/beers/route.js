95d366fa9fce61391abfbc975bad2590a03ff86c
Jeremie Roy <j-roy95@hotmail.com>
2018-06-06 14:03:17 -0400

    Cleanup

--- app/pods/beers/route.js
+++ app/pods/beers/route.js
@@ -3,12 +3,12 @@ import RouteQueryManager from 'ember-apollo-client/mixins/route-query-manager';
 import query from 'pinte-ball/queries/get-beers';
 
 export default Route.extend(RouteQueryManager, {
-     model() {
-        let variables = {
-          skip: 0,
-          first: 10
-        };
+  model() {
+    let variables = {
+      skip: 0,
+      first: 10
+    };
 
-        return this.get('apollo').query({query, variables});
-    }
+    return this.get('apollo').query({query, variables}, "beers");
+  }
 });
