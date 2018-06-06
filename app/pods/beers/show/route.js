94c35a1a4c4d47a6e7274f604d190c1f496f1c1a
Andy <andy1337@live.ca>
2018-06-06 18:06:08 -0400

    Made review changes (except for beer's type)

--- app/pods/beers/show/route.js
+++ app/pods/beers/show/route.js
@@ -1,6 +1,5 @@
 import Route from '@ember/routing/route';
 import { inject as service } from '@ember/service';
-import RouteQueryManager from 'ember-apollo-client/mixins/route-query-manager';
 import query from 'pinte-ball/queries/get-beer';
 
 export default Route.extend({
