1d724c25a52898cdfe235cc23c3c8042face0474
Andy <andy1337@live.ca>
2018-06-06 13:40:15 -0400

    Added a helper to round the rating and cleaned up

--- app/pods/components/bp-beer/component.js
+++ app/pods/components/bp-beer/component.js
@@ -1,9 +1,4 @@
 import Component from '@ember/component';
-import { computed } from '@ember/object';
 
 export default Component.extend({
-  beerRating: computed('beer', function() {
-    let rating = this.get('beer').rating;
-    return Math.round(rating * 10) / 10;
-  }),
 });
