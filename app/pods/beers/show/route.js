a2c1d9eb941f17dcfb62bfeef01fefa7a38f786f
Andy <alarochelle@users.noreply.github.com>
2018-06-24 18:40:24 -0400

    Add reviews to beer and brewery pages (#9)
    * Fixed missing css for beer page
    
    * Added simple reviews to beer and brewery pages and made a lot of css fixes/cleanup
    
    * Aligned content correctly on beer and brewery pages
    
    * Removed reviews.length cause it made no sense
    
    * Made even more css adjustments
    
    * Added ember-moment for time formatting
    
    * Removed useless tests

--- app/pods/beers/show/route.js
+++ app/pods/beers/show/route.js
@@ -1,15 +1,28 @@
 import Route from '@ember/routing/route';
 import { inject as service } from '@ember/service';
-import query from 'pinte-ball/queries/get-beer';
+import Beer from 'pinte-ball/queries/get-beer';
+import BeerReviews from 'pinte-ball/queries/get-beer-reviews';
+import RSVP from 'rsvp';
 
 export default Route.extend({
   apollo: service('apollo'),
 
   model(params) {
-    let variables = {
+    const beerVariables = {
       beerId: params.beer_id
     }
 
-    return this.get('apollo').query({ query, variables }, 'beer');
+    const reviewsVariables = {
+      skip: 0,
+      first: 50,
+      beers: [params.beer_id],
+      cips: []
+    }
+
+    return RSVP.hash({
+      beer: this.get('apollo').query({ query: Beer, variables: beerVariables }, 'beer'),
+      reviews: this.get('apollo').query({ query: BeerReviews, variables: reviewsVariables }, 'beerReviews')
+    });
   }
 });
+
--- app/pods/breweries/show/route.js
+++ app/pods/breweries/show/route.js
@@ -1,16 +1,29 @@
 import Route from '@ember/routing/route';
 import { inject as service } from '@ember/service';
-import query from 'pinte-ball/queries/get-brewery';
+import Brewery from 'pinte-ball/queries/get-brewery';
+import BreweryReviews from 'pinte-ball/queries/get-brewery-reviews';
+import RSVP from 'rsvp';
 
 export default Route.extend({
   apollo: service('apollo'),
 
   model(params) {
-    let variables = {
+    const breweryVariables = {
       breweryId: params.brewery_id,
       breweryIdArray: [params.brewery_id]
     }
 
-    return this.get('apollo').query({ query, variables });
+    const reviewsVariables = {
+      skip: 0,
+      first: 50,
+      breweries: [params.brewery_id],
+      cips: []
+    }
+
+
+    return RSVP.hash({
+      brewery: this.get('apollo').query({ query: Brewery, variables:breweryVariables }),
+      reviews: this.get('apollo').query({ query: BreweryReviews, variables: reviewsVariables }, 'breweryReviews')
+    });
   }
 });
