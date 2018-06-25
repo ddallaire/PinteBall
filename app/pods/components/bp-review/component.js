a54f2ddc925ed39daa6b2886ef2d658b04f066e9
Andy <alarochelle@users.noreply.github.com>
2018-06-25 17:49:37 -0400

    Add review comments (#10)
    * Added review comments for beers
    
    * Added brewery review comments
    
    * Cleaned up if statement
    
    * Rename bp-beer-review-comment to bp-review-comment

--- app/pods/components/bp-review/component.js
+++ app/pods/components/bp-review/component.js
@@ -1,4 +1,34 @@
 import Component from '@ember/component';
+import { inject as service } from '@ember/service';
+import BeerReviewComments from 'pinte-ball/queries/get-beer-review-comments';
+import BreweryReviewComments from 'pinte-ball/queries/get-brewery-review-comments';
+import { computed } from '@ember/object';
 
 export default Component.extend({
+  apollo: service('apollo'),
+  comments: computed('review', function() {
+    let variables = {
+      skip: 0,
+      first: 50,
+
+      cips: []
+    }
+
+    let query;
+    let responseName;
+
+    if (this.get('type') === 'beer') {
+      variables.beerReviews = [this.get('review.idBeerReview')];
+      query = BeerReviewComments;
+      responseName = 'beerReviewComments'
+    } else {
+      variables.breweryReviews = [this.get('review.idBreweryReview')];
+      query = BreweryReviewComments;
+      responseName = 'breweryReviewComments';
+    }
+
+    Promise.resolve(this.get('apollo').query({ query, variables }, responseName)).then(c => {
+      this.set('comments', c);
+    });
+  })
 });
