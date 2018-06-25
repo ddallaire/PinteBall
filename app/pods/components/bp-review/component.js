0cd09ec89d21c832914f3ffb224fa5277fdbc894
Andy <andy1337@live.ca>
2018-06-25 17:30:40 -0400

    Added brewery review comments

--- app/pods/components/bp-review/component.js
+++ app/pods/components/bp-review/component.js
@@ -1,19 +1,29 @@
 import Component from '@ember/component';
 import { inject as service } from '@ember/service';
 import BeerReviewComments from 'pinte-ball/queries/get-beer-review-comments';
+import BreweryReviewComments from 'pinte-ball/queries/get-brewery-review-comments';
 import { computed } from '@ember/object';
 
 export default Component.extend({
   apollo: service('apollo'),
   comments: computed('review', function() {
-    const variables = {
+    let variables = {
       skip: 0,
       first: 50,
-      beerReviews: [this.get('review.idBeerReview')],
+
       cips: []
     }
-    Promise.resolve(this.get('apollo').query({ query: BeerReviewComments, variables }, 'beerReviewComments')).then(c => {
-      this.set('comments', c);
-    });
+
+    if (this.get('type') === 'beer') {
+      variables.beerReviews = [this.get('review.idBeerReview')];
+      Promise.resolve(this.get('apollo').query({ query: BeerReviewComments, variables }, 'beerReviewComments')).then(c => {
+        this.set('comments', c);
+      });
+    } else {
+      variables.breweryReviews = [this.get('review.idBreweryReview')];
+      Promise.resolve(this.get('apollo').query({ query: BreweryReviewComments, variables }, 'breweryReviewComments')).then(c => {
+        this.set('comments', c);
+      });
+    }
   })
 });
