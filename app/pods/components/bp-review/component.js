877b183a4815714bf590e7a3b9da92b7156916a9
Andy <andy1337@live.ca>
2018-06-25 17:25:26 -0400

    Added review comments for beers

--- app/pods/components/bp-review/component.js
+++ app/pods/components/bp-review/component.js
@@ -1,4 +1,19 @@
 import Component from '@ember/component';
+import { inject as service } from '@ember/service';
+import BeerReviewComments from 'pinte-ball/queries/get-beer-review-comments';
+import { computed } from '@ember/object';
 
 export default Component.extend({
+  apollo: service('apollo'),
+  comments: computed('review', function() {
+    const variables = {
+      skip: 0,
+      first: 50,
+      beerReviews: [this.get('review.idBeerReview')],
+      cips: []
+    }
+    Promise.resolve(this.get('apollo').query({ query: BeerReviewComments, variables }, 'beerReviewComments')).then(c => {
+      this.set('comments', c);
+    });
+  })
 });
