4f30f9ae023a74927c1b847df994c650f354e3ad
ddallaire <ddallaire25@gmail.com>
2018-06-26 08:26:10 -0400

    Add filters to beer list

--- app/pods/components/bp-beer-list/component.js
+++ app/pods/components/bp-beer-list/component.js
@@ -2,24 +2,53 @@ import Component from '@ember/component'
 import { computed } from '@ember/object';
 import { inject as service } from '@ember/service';
 import beersQuery from 'pinte-ball/queries/get-beers';
+import beerStylesQuery from 'pinte-ball/queries/get-beer-styles';
+import tagsQuery from 'pinte-ball/queries/get-tags';
 
 export default Component.extend({
   apollo: service('apollo'),
 
   beers: null,
+  beerStyles: null,
+  beerTags: null,
 
-  queryVariables: computed(function() {
+  init() {
+    this._super(...arguments);
+    this.set('styleFilters', []);
+    this.set('tagFilters', []);
+  },
+
+  queryVariables: computed('styleFilters.[]', 'tagFilters.[]', function() {
+    const styleFilters = this.get('styleFilters').map(style => style.id);
+    const tagFilters = this.get('tagFilters').map(tag => tag.id);
+
     return {
       skip: 0,
-      first: 10
+      first: 10,
+      styles: styleFilters,
+      tags: tagFilters
     };
   }),
 
-  filters: computed(function() {
-    return null;
+  beerTagsQuery: computed(function() {
+    return this.get('apollo').query({
+      query: tagsQuery,
+      variables: {skip: 0, first: 20}
+    }, "tags").then(result => {
+      this.set('beerTags', result);
+    });
   }),
 
-  beersQuery: computed('filters', function() {
+  beerStylesQuery: computed(function() {
+    return this.get('apollo').query({
+      query: beerStylesQuery,
+      variables: {skip: 0, first: 20}
+    }, "beerStyles").then(result => {
+      this.set('beerStyles', result);
+    });
+  }),
+
+  beersQuery: computed('queryVariables', function() {
     return this.get('apollo').query({
       query: beersQuery,
       variables: this.get('queryVariables')
