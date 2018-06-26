55caa2fe37282c04be99cae8d5388b9953f60716
ddallaire <ddallaire25@gmail.com>
2018-06-25 23:16:24 -0400

    Remove unecessary comma

--- ember-cli-build.js
+++ ember-cli-build.js
@@ -6,7 +6,7 @@ module.exports = function(defaults) {
   let app = new EmberApp(defaults, {
     apollo: {
       include: ['apollo-link-error']
-    },
+    }
   });
 
   // Use `app.import` to add additional libraries to the generated
