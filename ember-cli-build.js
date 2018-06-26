a05970b7e98d323bf2ef6ee3efea2893fc53913e
ddallaire <ddallaire25@gmail.com>
2018-06-25 23:14:31 -0400

    Restart authentification process when receiving 401

--- ember-cli-build.js
+++ ember-cli-build.js
@@ -4,6 +4,9 @@ const EmberApp = require('ember-cli/lib/broccoli/ember-app');
 
 module.exports = function(defaults) {
   let app = new EmberApp(defaults, {
+    apollo: {
+      include: ['apollo-link-error']
+    },
   });
 
   // Use `app.import` to add additional libraries to the generated
