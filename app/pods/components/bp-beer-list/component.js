c8d2e664baf2272859b0b8d48293e485bdd69625
ddallaire <ddallaire25@gmail.com>
2018-06-26 01:06:26 -0400

    Refactor beer list page

--- app/pods/components/bp-beer-list/component.js
+++ app/pods/components/bp-beer-list/component.js
@@ -1,4 +1,30 @@
 import Component from '@ember/component'
+import { computed } from '@ember/object';
+import { inject as service } from '@ember/service';
+import beersQuery from 'pinte-ball/queries/get-beers';
 
 export default Component.extend({
+  apollo: service('apollo'),
+
+  beers: null,
+
+  queryVariables: computed(function() {
+    return {
+      skip: 0,
+      first: 10
+    };
+  }),
+
+  filters: computed(function() {
+    return null;
+  }),
+
+  beersQuery: computed('filters', function() {
+    return this.get('apollo').query({
+      query: beersQuery,
+      variables: this.get('queryVariables')
+    }, "beers").then(result => {
+      this.set('beers', result);
+    });
+  })
 });
