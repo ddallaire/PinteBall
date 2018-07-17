563ded558a90ad5967db63c2e129af48349f25fa
Jeremie Roy <j-roy95@hotmail.com>
2018-07-17 14:17:55 -0400

    Add thumbsup functionality

--- app/pods/components/bp-review/component.js
+++ app/pods/components/bp-review/component.js
@@ -4,12 +4,23 @@ import { inject as service } from '@ember/service';
 import { getObservable } from 'ember-apollo-client';
 import BeerReviewComments from 'pinte-ball/queries/get-beer-review-comments';
 import BreweryReviewComments from 'pinte-ball/queries/get-brewery-review-comments';
+import InsertBreweryReviewThumbsup from 'pinte-ball/queries/mutations/insert-brewery-review-thumbsup';
+import DeleteBreweryReviewThumbsup from 'pinte-ball/queries/mutations/delete-brewery-review-thumbsup';
+import InsertBeerReviewThumbsup from 'pinte-ball/queries/mutations/insert-beer-review-thumbsup';
+import DeleteBeerReviewThumbsup from 'pinte-ball/queries/mutations/delete-beer-review-thumbsup';
 
 export default Component.extend({
   apollo: service('apollo'),
+  session: service('session'),
   displayAddComment: false,
+  liked: false,
 
   comments: null,
+
+  liked: computed('review', 'session', function() {
+    const reviews = this.get('review.thumbsups');
+    return !!reviews.find(a => a.user.cip == this.get('session').credentials.cip);
+  }),
 
   commentsQuery: computed(function() {
     let query = null;
@@ -50,6 +61,23 @@ export default Component.extend({
     refetchComments() {
       this.toggleProperty('displayAddComment');
       getObservable(this.get('comments')).refetch();
+    },
+    toggleLike: function() {
+      let query = null;
+      let variables = null;
+      let insert = false;
+      let liked = this.get('liked');
+      this.set('liked', !liked);
+
+      if (this.get('type') === 'beer') {
+        query = liked ? DeleteBeerReviewThumbsup : InsertBeerReviewThumbsup;
+        variables = {id: this.get('review.idBeerReview')};
+      } else {
+        query = liked ?  DeleteBreweryReviewThumbsup : InsertBreweryReviewThumbsup;
+        variables = {id: this.get('review.idBreweryReview')};
+      }
+
+      this.apollo.client.mutate({mutation: query, variables}).then(() => {});
     }
   },
 });
