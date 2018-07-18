cbb037edd1ac7ba3d5d80de9ad7e0e917162cef7
Andy <andy1337@live.ca>
2018-07-18 13:33:55 -0400

    Removed mandatory image path from add review and changed Rating to Rating /5

--- app/validations/add-review.js
+++ app/validations/add-review.js
@@ -3,6 +3,5 @@ import { validatePresence, validateLength, validateNumber } from 'ember-changese
 export default {
   reviewTitle: [validatePresence(true), validateLength({ min: 1, max: 256 })],
   reviewRating: [validatePresence(true), validateNumber({ positive: true, lte: 5.0, gte: 0.0, integer: false })],
-  reviewContent: [validatePresence(true), validateLength({ min: 1, max: 1024 })],
-  reviewImagePath: [validatePresence(true), validateLength({ min: 1, max: 1024 })],
+  reviewContent: [validatePresence(true), validateLength({ min: 1, max: 1024 })]
 }
