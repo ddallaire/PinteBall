131e74f6b258ea746cb074ec6835942bd25375c9
Jeremie Roy <j-roy95@hotmail.com>
2018-07-23 18:03:06 -0400

    Clean up code

--- app/pods/components/bp-filter/component.js
+++ app/pods/components/bp-filter/component.js
@@ -1,17 +1,4 @@
 import Component from '@ember/component';
 
 export default Component.extend({
-    test: [],
-    selected: [],
-    
-    init: function() {
-        this._super(...arguments);
-        this.set('test', [{id: 1, title: 'test'}, {id: 2, title: 'test2'}]);
-    },
-
-    actions: {
-        update(selection) {
-            console.log(selection);
-        }
-    }
 });
